47a96a6125bdf7e0417477853f61be20ecc29f75
vivienkp <vivienkp@gmail.com>
2023-08-02 10:22:07 -0400

    user authenthication added with dynamic nav bar

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+# User Auth Model below:
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Goal(models.Model):
     title = models.CharField(max_length=255)
+    # One user --< has many goals therefore goals has FK 
+    # user authen linking a goal to a user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
